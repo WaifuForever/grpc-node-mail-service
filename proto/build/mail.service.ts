13e8861fad0e18c156284037d77701ae7b32f9ee
Jojo <sirjoandersonwf@gmail.com>
2022-04-14 08:33:06 -0300

    fix: type error on package name

--- proto/build/mail.service.ts
+++ proto/build/mail.service.ts
@@ -1,17 +1,25 @@
 import type * as grpc from '@grpc/grpc-js';
 import type { MessageTypeDefinition } from '@grpc/proto-loader';
 
-import type { MailServiceClient as _mails_MailServiceClient, MailServiceDefinition as _mails_MailServiceDefinition } from './mails/MailService';
+import type {
+    MailServiceClient as _mails_MailServiceClient,
+    MailServiceDefinition as _mails_MailServiceDefinition,
+} from './mails/MailService';
 
-type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
-  new(...args: ConstructorParameters<Constructor>): Subtype;
+type SubtypeConstructor<
+    Constructor extends new (...args: any) => any,
+    Subtype,
+> = {
+    new (...args: ConstructorParameters<Constructor>): Subtype;
 };
 
 export interface ProtoGrpcType {
-  sendMail: {
-    MailRequest: MessageTypeDefinition
-    MailResponse: MessageTypeDefinition
-    MailService: SubtypeConstructor<typeof grpc.Client, _mails_MailServiceClient> & { service: _mails_MailServiceDefinition }
-  }
+    mails: {
+        MailRequest: MessageTypeDefinition;
+        MailResponse: MessageTypeDefinition;
+        MailService: SubtypeConstructor<
+            typeof grpc.Client,
+            _mails_MailServiceClient
+        > & { service: _mails_MailServiceDefinition };
+    };
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import { loadPackageDefinition, Server, ServerCredentials } from "@grpc/grpc-js";
-import * as protoLoader from "@grpc/proto-loader"
+import {
+    loadPackageDefinition,
+    Server,
+    ServerCredentials,
+} from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
 
 import { ProtoGrpcType as MailService } from '../proto/build/mail.service';
-import MailController from "./controller/mail.controller";
-const PROTO_PATH = "../proto/services/mail.service.proto";
-
+import MailController from './controller/mail.controller';
+const PROTO_PATH = './proto/services/mail.service.proto';
 
 const host = process.env.HOST ? process.env.HOST : 'localhost:5000';
 
 function getServer(): Server {
     const proto = loadPackageDefinition(
-        protoLoader.loadSync('./proto/services/mail.service.proto'),
+        protoLoader.loadSync(PROTO_PATH),
     ) as unknown as MailService;
     const server = new Server();
-    server.addService(proto.sendMail.MailService.service, MailController);
+    server.addService(proto.mails.MailService.service, MailController);
     return server;
 }
 
